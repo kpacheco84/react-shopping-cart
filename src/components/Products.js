5a65e7417434f67a7a566b9e559f649235870312
Katherine <katherinepacheco001@gmail.com>
2020-08-30 08:53:40 -0400

    css fix of images

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,18 +8,20 @@ export default class products extends Component {
         <ul className="products">
           {this.props.products.map((product) => (
             <li key={product._id}>
-              <a href={"#" + product._id}>
-                <img src={product.image} alt={product.title}></img>
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div>{formatCurrency(product.price)}</div>
-                <button
-                  onClick={() => this.props.addToCart(product)}
-                  className="button-primary"
-                >
-                  Add To Cart
-                </button>
+              <div className="product">
+                <a href={"#" + product._id}>
+                  <img src={product.image} alt={product.title}></img>
+                  <p>{product.title}</p>
+                </a>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button
+                    onClick={() => this.props.addToCart(product)}
+                    className="button-primary"
+                  >
+                    Add To Cart
+                  </button>
+                </div>
               </div>
             </li>
           ))}
